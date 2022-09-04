b265efa597c8eb6a98cdae997ce6f0b55a215850
Shivamji1234 <108694517+Shivamji1234@users.noreply.github.com>
2022-09-04 06:57:08 +0530

    Add files via upload

---
+++ Check whether a given Binary Tree is Complete or not.py
@@ -0,0 +1,43 @@
+from unittest import result
+
+
+class Node:
+    def __init__(self,key):
+        self.key=key
+        self.left=None
+        self.right=None
+
+class binary:
+    def __init__(self):
+        self.root=None
+    
+    def comp(self,root):
+        if root is None:
+            return True
+        q=[root]
+        nq=[]
+        level=[]
+        result=[]
+        while len(q)>0:
+            for root in q:
+                level.append(root.key)
+                if root.left is not None:
+                    nq.append(root.left)
+                if root.right is not None:
+                    nq.append(root.right)
+                if root.left is None and root.right is not None:
+                    return False, "not a complete binary tree"
+            result.append(level)
+            level=[]
+            q=nq
+            nq=[]
+        return True,"complete binary tree"
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.right = Node(6)
+l=binary()
+print(l.comp(root))
